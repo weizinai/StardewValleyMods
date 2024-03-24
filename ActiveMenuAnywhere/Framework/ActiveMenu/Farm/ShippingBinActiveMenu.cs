8c22abc4354a913ce6dc15e58f8a3de2bc7933a1
weizinai <1776061683@qq.com>
2024-03-24 23:46:18 +0800

    main

--- ActiveMenuAnywhere/Framework/ActiveMenu/Farm/ShippingBinActiveMenu.cs
+++ ActiveMenuAnywhere/Framework/ActiveMenu/Farm/ShippingBinActiveMenu.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
+using StardewValley;
 using StardewValley.Buildings;
+using StardewValley.Objects;
 
 namespace ActiveMenuAnywhere.Framework.ActiveMenu.Farm;
 
@@ -16,6 +18,6 @@ public class ShippingBinActiveMenu: BaseActiveMenu
 
     public override void ReceiveLeftClick()
     {
-        helper.Reflection.GetMethod(new ShippingBin(), "doAction").Invoke();
+        helper.Reflection.GetMethod(new ShippingBin(), "leftClicked").Invoke(new Vector2(1,1), Game1.player);
     }
 }

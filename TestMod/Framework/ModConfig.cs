92a95b751f1f598474fe0668a2cad58a18a26c5d
weizinai <1776061683@qq.com>
2024-04-24 16:54:21 +0800

    删除测试内容

--- TestMod/Framework/ModConfig.cs
+++ TestMod/Framework/ModConfig.cs
@@ -4,13 +4,4 @@ public class ModConfig
 {
     public int MineShaftMap { get; set; } = 40;
     public int VolcanoDungeonMap { get; set; } = 46;
-    
-    // 显示梯子信息
-    public bool ShowLadderInfo { get; set; } = true;
-    // 显示竖井信息
-    public bool ShowShaftInfo { get; set; } = true;
-    // 显示怪物信息
-    public bool ShowMonsterInfo { get; set; } = true;
-    // 显示矿物信息
-    public bool ShowMineralInfo { get; set; } = true;
 }

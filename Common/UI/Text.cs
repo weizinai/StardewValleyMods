db353b7ad470bf5270fad08131485c13f7a8c73a
weizinai <1776061683@qq.com>
2024-04-25 20:21:59 +0800

    移除无用逻辑

--- Common/UI/Text.cs
+++ Common/UI/Text.cs
@@ -12,26 +12,22 @@ public sealed class Text : Element
 
     private readonly SpriteFont font;
     private readonly string text;
-    private readonly Color idleColor;
-    private readonly Color hoverColor;
+    private readonly Color color;
     private readonly float scale;
 
-    public Text(string text, Vector2 localPosition, SpriteFont? font = null, Color? idleColor = null, Color? hoverColor = null, float scale = 1f)
+    public Text(string text, Vector2 localPosition, SpriteFont? font = null, Color? color = null, float scale = 1f)
     {
-        this.font = font ?? Game1.dialogueFont;
+        this.font = font ?? Game1.temporaryContent.Load<SpriteFont>("Fonts/SpriteFont1");
         this.text = text;
         LocalPosition = localPosition;
-        this.idleColor = idleColor ?? Game1.textColor;
-        this.hoverColor = hoverColor ?? Game1.unselectedOptionColor;
+        this.color = color ?? Game1.textColor;
         this.scale = scale;
     }
 
     public override void Draw(SpriteBatch spriteBatch)
     {
         if (IsHidden()) return;
-
-        var altColor = Hover && OnHover is not null;
-        var color = altColor ? hoverColor : idleColor;
+        
         spriteBatch.DrawString(font, text, Position, color, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
     }
 

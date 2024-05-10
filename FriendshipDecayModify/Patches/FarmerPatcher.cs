2771fa2790b066dd712b8f30ed61e168275e552c
weizinai <1776061683@qq.com>
2024-05-10 15:30:05 +0800

    每日对话修改

--- FriendshipDecayModify/Patches/FarmerPatcher.cs
+++ FriendshipDecayModify/Patches/FarmerPatcher.cs
@@ -2,38 +2,54 @@ using Common.Patch;
 using FriendshipDecayModify.Framework;
 using HarmonyLib;
 using StardewValley;
+using StardewValley.Characters;
 
 namespace FriendshipDecayModify.Patches;
 
 public class FarmerPatcher : BasePatcher
 {
     private static ModConfig config = null!;
-        
+
     public FarmerPatcher(ModConfig config)
     {
         FarmerPatcher.config = config;
     }
-    
+
     public override void Patch(Harmony harmony)
     {
         harmony.Patch(
             RequireMethod<Farmer>(nameof(Farmer.resetFriendshipsForNewDay)),
-            transpiler: GetHarmonyMethod(nameof(ResetFriendshipsForNewDayTranspiler))
+            prefix: GetHarmonyMethod(nameof(ResetFriendshipsForNewDayPrefix))
         );
     }
 
-    // 每日对话修改
-    private static IEnumerable<CodeInstruction> ResetFriendshipsForNewDayTranspiler(IEnumerable<CodeInstruction> instructions)
+    private static bool ResetFriendshipsForNewDayPrefix(Farmer __instance)
     {
-        var codes = instructions.ToList();
+        foreach (var name in __instance.friendshipData.Keys)
+        {
+            var npc = Game1.getCharacterFromName(name) ?? Game1.getCharacterFromName<Child>(name, false);
+            if (npc is null) continue;
+            
+            var single = npc.datable.Value && !__instance.friendshipData[name].IsDating() && !npc.isMarried();
+            var talkToNPC = __instance.hasPlayerTalkedToNPC(name);
+            var points = __instance.friendshipData[name].Points;
 
-        var index = codes.FindIndex(code => code.operand.Equals(-20));
-        codes[index].operand = config.DailyGreetingModifyForSpouse;
-        index = codes.FindIndex(code => code.operand.Equals(-8));
-        codes[index].operand = config.DailyGreetingModifyForDatingVillager;
-        index = codes.FindIndex(code => code.operand.Equals(-2));
-        codes[index].operand = config.DailyGreetingModifyForVillager;
+            if (talkToNPC)
+            {
+                __instance.friendshipData[name].TalkedToToday = false;
+            }
+            else
+            {
+                if (__instance.spouse == name)
+                    __instance.changeFriendship(-config.DailyGreetingModifyForSpouse, npc);
+                else if (__instance.friendshipData[name].IsDating() && points < 2500)
+                    __instance.changeFriendship(-config.DailyGreetingModifyForDatingVillager, npc);
+                else if ((!single && points < 2500) || (single && points < 2000))
+                    __instance.changeFriendship(-config.DailyGreetingModifyForVillager, npc);
+            }
+        }
 
-        return codes.AsEnumerable();
+        __instance.updateFriendshipGifts(Game1.Date);
+        return false;
     }
 }

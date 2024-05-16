3a44da07d7d3dee9c4b6fb7990ef65992f51653f
weizinai <1776061683@qq.com>
2024-05-16 15:57:45 +0800

    添加QuestNote类

--- HelpWanted/Framework/QuestNote.cs
+++ HelpWanted/Framework/QuestNote.cs
@@ -1,6 +1,5 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using StardewValley;
 using StardewValley.Menus;
 
 namespace HelpWanted.Framework;
@@ -16,6 +15,9 @@ internal class QuestNote : ClickableComponent
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        
+        spriteBatch.Draw(questData.PadTexture, bounds, questData.PadTextureSource, questData.PadColor);
+        spriteBatch.Draw(questData.PinTexture, bounds, questData.PinTextureSource, questData.PinColor);
+        spriteBatch.Draw(questData.Icon, new Vector2(bounds.X + questData.IconOffset.X, bounds.Y + questData.IconOffset.Y), questData.IconSource, questData.IconColor,
+            0,Vector2.Zero,questData.IconScale,SpriteEffects.None,0);
     }
 }

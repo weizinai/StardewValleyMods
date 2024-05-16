c90a8ebedd810a153ab24166fafd3129dd8da566
weizinai <1776061683@qq.com>
2024-05-16 16:01:01 +0800

    将QuestData设为Public

--- HelpWanted/Framework/QuestNote.cs
+++ HelpWanted/Framework/QuestNote.cs
@@ -6,18 +6,18 @@ namespace HelpWanted.Framework;
 
 internal class QuestNote : ClickableComponent
 {
-    private readonly QuestData questData;
+    public readonly QuestData QuestData;
 
     public QuestNote(QuestData questData, Rectangle bounds): base(bounds, "")
     {
-        this.questData = questData;
+        QuestData = questData;
     }
 
     public void Draw(SpriteBatch spriteBatch)
     {
-        spriteBatch.Draw(questData.PadTexture, bounds, questData.PadTextureSource, questData.PadColor);
-        spriteBatch.Draw(questData.PinTexture, bounds, questData.PinTextureSource, questData.PinColor);
-        spriteBatch.Draw(questData.Icon, new Vector2(bounds.X + questData.IconOffset.X, bounds.Y + questData.IconOffset.Y), questData.IconSource, questData.IconColor,
-            0,Vector2.Zero,questData.IconScale,SpriteEffects.None,0);
+        spriteBatch.Draw(QuestData.PadTexture, bounds, QuestData.PadTextureSource, QuestData.PadColor);
+        spriteBatch.Draw(QuestData.PinTexture, bounds, QuestData.PinTextureSource, QuestData.PinColor);
+        spriteBatch.Draw(QuestData.Icon, new Vector2(bounds.X + QuestData.IconOffset.X, bounds.Y + QuestData.IconOffset.Y), QuestData.IconSource, QuestData.IconColor,
+            0,Vector2.Zero,QuestData.IconScale,SpriteEffects.None,0);
     }
 }

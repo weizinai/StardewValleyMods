764b58b94a9d489e0d65d8817018af6f1c4e30e7
weizinai <1776061683@qq.com>
2024-05-16 12:00:29 +0800

    使用GenericModConfigMenuIntegration重构GenericModConfigMenu

--- FriendshipDecayModify/ModEntry.cs
+++ FriendshipDecayModify/ModEntry.cs
@@ -1,5 +1,4 @@
-﻿using Common.Integrations;
-using Common.Patch;
+﻿using Common.Patch;
 using FriendshipDecayModify.Framework;
 using FriendshipDecayModify.Patches;
 using StardewModdingAPI;
@@ -24,80 +23,12 @@ internal class ModEntry : Mod
 
     private void OnGameLaunched(object? sender, GameLaunchedEventArgs e)
     {
-        var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
-        if (configMenu is null) return;
-
-        configMenu.Register(ModManifest, () => config = new ModConfig(), () => Helper.WriteConfig(config));
-
-        // 每日对话修改
-        configMenu.AddSectionTitle(ModManifest, I18n.Config_DailyGreetingModifyTitle_Name);
-        configMenu.AddNumberOption(
+        new GenericModConfigMenuIntegrationForFriendshipDecayModify(
+            Helper,
             ModManifest,
-            () => config.DailyGreetingModifyForVillager,
-            value => config.DailyGreetingModifyForVillager = value,
-            I18n.Config_DailyGreetingModifyForVillager_Name
-        );
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.DailyGreetingModifyForDatingVillager,
-            value => config.DailyGreetingModifyForDatingVillager = value,
-            I18n.Config_DailyGreetingModifyForDatingVillager_Name
-        );
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.DailyGreetingModifyForSpouse,
-            value => config.DailyGreetingModifyForSpouse = value,
-            I18n.Config_DailyGreetingModifyForSpouse_Name
-        );
-        // 礼物修改
-        configMenu.AddSectionTitle(ModManifest, I18n.Config_GiftModifyTitle_Name);
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.DislikeGiftModify,
-            value => config.DislikeGiftModify = value,
-            I18n.Config_DislikeGiftModify_Name
-        );
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.HateGiftModify,
-            value => config.HateGiftModify = value,
-            I18n.Config_HateGiftModify_Name
-        );
-        // 垃圾桶修改
-        configMenu.AddSectionTitle(ModManifest, I18n.Config_GarbageCanModify_Name);
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.GarbageCanModify,
-            value => config.GarbageCanModify = value,
-            I18n.Config_GarbageCanModify_Name
-        );
-        // 动物好感度修改
-        configMenu.AddSectionTitle(ModManifest, I18n.Config_PetAnimalModifyForFriendship_Name);
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.PetAnimalModifyForFriendship,
-            value => config.PetAnimalModifyForFriendship = value,
-            I18n.Config_PetAnimalModifyForFriendship_Name
-        );
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.FeedAnimalModifyForFriendship,
-            value => config.FeedAnimalModifyForFriendship = value,
-            I18n.Config_FeedAnimalModifyForFriendship_Name
-        );
-        // 动物心情修改
-        configMenu.AddSectionTitle(ModManifest, I18n.Config_PetAnimalModifyForHappiness_Name);
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.PetAnimalModifyForHappiness,
-            value => config.PetAnimalModifyForHappiness = value,
-            I18n.Config_PetAnimalModifyForHappiness_Name
-        );
-        configMenu.AddNumberOption(
-            ModManifest,
-            () => config.FeedAnimalModifyForHappiness,
-            value => config.FeedAnimalModifyForHappiness = value,
-            I18n.Config_FeedAnimalModifyForHappiness_Name
-        );
+            () => config,
+            () => config = new ModConfig(),
+            () => Helper.WriteConfig(config)
+        ).Register();
     }
 }

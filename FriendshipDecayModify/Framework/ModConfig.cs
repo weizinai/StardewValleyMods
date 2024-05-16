872040d076bf6a7b3381c0794dc9f15b1d5a0c80
weizinai <1776061683@qq.com>
2024-05-16 11:45:58 +0800

    格式化

--- FriendshipDecayModify/Framework/ModConfig.cs
+++ FriendshipDecayModify/Framework/ModConfig.cs
@@ -1,5 +1,3 @@
-// ReSharper disable FieldCanBeMadeReadOnly.Global
-
 namespace FriendshipDecayModify.Framework;
 
 public class ModConfig
@@ -7,22 +5,15 @@ public class ModConfig
     // 每日对话修改
     public int DailyGreetingModifyForVillager = 2;
     public int DailyGreetingModifyForDatingVillager = 8;
-
     public int DailyGreetingModifyForSpouse = 20;
-
     // 礼物修改
     public int DislikeGiftModify = 20;
-
     public int HateGiftModify = 40;
-
     // 垃圾桶修改
     public int GarbageCanModify = 25;
-
     // 动物友谊修改
     public int PetAnimalModifyForFriendship = 10;
-
     public int FeedAnimalModifyForFriendship = 20;
-
     // 动物心情修改
     public int PetAnimalModifyForHappiness = 50;
     public int FeedAnimalModifyForHappiness = 100;

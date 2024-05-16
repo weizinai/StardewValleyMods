53456e77b1d84b182c366353a46d243d7f5223a6
weizinai <1776061683@qq.com>
2024-05-16 15:13:46 +0800

    使用属性

--- FriendshipDecayModify/Framework/ModConfig.cs
+++ FriendshipDecayModify/Framework/ModConfig.cs
@@ -5,20 +5,25 @@ namespace FriendshipDecayModify.Framework;
 
 internal class ModConfig
 {
-    public KeybindList OpenConfigMenu = new(SButton.None);
+    public KeybindList OpenConfigMenu { get; set; } = new(SButton.None);
+
     // 每日对话修改
-    public int DailyGreetingModifyForVillager = 2;
-    public int DailyGreetingModifyForDatingVillager = 8;
-    public int DailyGreetingModifyForSpouse = 20;
+    public int DailyGreetingModifyForVillager { get; set; } = 2;
+    public int DailyGreetingModifyForDatingVillager { get; set; } = 8;
+    public int DailyGreetingModifyForSpouse { get; set; } = 20;
+
     // 礼物修改
-    public int DislikeGiftModify = 20;
-    public int HateGiftModify = 40;
+    public int DislikeGiftModify { get; set; } = 20;
+    public int HateGiftModify { get; set; } = 40;
+
     // 垃圾桶修改
-    public int GarbageCanModify = 25;
+    public int GarbageCanModify { get; set; } = 25;
+
     // 动物友谊修改
-    public int PetAnimalModifyForFriendship = 10;
-    public int FeedAnimalModifyForFriendship = 20;
+    public int PetAnimalModifyForFriendship { get; set; } = 10;
+    public int FeedAnimalModifyForFriendship { get; set; } = 20;
+
     // 动物心情修改
-    public int PetAnimalModifyForHappiness = 50;
-    public int FeedAnimalModifyForHappiness = 100;
+    public int PetAnimalModifyForHappiness { get; set; } = 50;
+    public int FeedAnimalModifyForHappiness { get; set; } = 100;
 }

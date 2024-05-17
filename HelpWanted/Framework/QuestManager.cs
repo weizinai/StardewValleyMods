7951603f712c220e3ee29735ba401d743b4725b1
weizinai <1776061683@qq.com>
2024-05-17 15:08:18 +0800

    重构GetRSVQuest方法

--- HelpWanted/Framework/QuestManager.cs
+++ HelpWanted/Framework/QuestManager.cs
@@ -13,7 +13,6 @@ namespace HelpWanted.Framework;
 internal class QuestManager
 {
     private readonly ModConfig config;
-    private readonly IModHelper helper;
     private readonly AppearanceManager appearanceManager;
 
     public static readonly List<QuestData> VanillaQuestList = new();
@@ -22,7 +21,6 @@ internal class QuestManager
     public QuestManager(IModHelper helper, ModConfig config)
     {
         this.config = config;
-        this.helper = helper;
         appearanceManager = new AppearanceManager(helper, config);
     }
 
@@ -30,22 +28,23 @@ internal class QuestManager
     {
         if (Game1.stats.DaysPlayed <= 1 && !config.QuestFirstDay)
         {
-            Log.Trace("今天是游戏第一天,不生成任务.");
+            Log.Info("Today is the first day of the game, no daily quests are generated.");
             return;
         }
 
         if ((Utility.isFestivalDay() || Utility.isFestivalDay(Game1.dayOfMonth + 1, Game1.season)) && !config.QuestFestival)
         {
-            Log.Trace("今天或明天是节日,不生成任务.");
+            Log.Info("Today or tomorrow is the festival day, no daily quests are generated.");
             return;
         }
 
         if (Game1.random.NextDouble() >= config.DailyQuestChance)
         {
-            Log.Trace("今天不生成任务.");
+            Log.Trace("No daily quests are generated.");
             return;
         }
-        Log.Trace("初始化原版任务列表.");
+
+        Log.Info("Begin generating today's daily quests.");
         ItemDeliveryQuestPatcher.Init();
         VanillaQuestList.Clear();
         var quest = GetVanillaQuest();
@@ -78,11 +77,13 @@ internal class QuestManager
 
             quest = GetVanillaQuest();
         }
+
+        Log.Info("End generating today's daily quests.");
     }
 
     public void InitRSVQuestList()
     {
-        Log.Trace("初始化RSV任务列表.");
+        Log.Info("Begin generating today's daily quests of RSV.");
         RSVQuestList.Clear();
         var quest = GetRSVQuest();
         for (var i = 0; i < 5; i++)
@@ -97,6 +98,8 @@ internal class QuestManager
 
             quest = GetRSVQuest();
         }
+
+        Log.Info("End generating today's daily quests of RSV.");
     }
 
     private NPC? GetNpcFromQuest(Quest quest)
@@ -162,7 +165,7 @@ internal class QuestManager
     private Quest? GetVanillaQuest()
     {
         Quest? quest = null;
-        
+
         var randomDouble = Game1.random.NextDouble();
         var slayMonsterQuest = MineShaft.lowestLevelReached > 0 && Game1.stats.DaysPlayed > 5U;
         var questTypes = new List<(float weight, Func<Quest> createQuest)>
@@ -179,7 +182,7 @@ internal class QuestManager
             currentWeight += weight;
             if (randomDouble < currentWeight / totalWeight) quest = createQuest();
         }
-        
+
         if (quest is null)
         {
             Log.Warn("Get Vanilla Quest Failed.");
@@ -199,139 +202,9 @@ internal class QuestManager
 
     private Quest? GetRSVQuest()
     {
-        return Game1.random.NextBool() ? GetRandomHandCraftedQuest() : GetRSVFishingQuest();
-    }
-    
-    private Quest? GetRandomHandCraftedQuest()
-    {
-        var quests = DataLoader.Quests(Game1.content);
-        var candidates = new List<string>();
-        foreach(var key in quests.Keys)
-        {
-            if (int.TryParse(key, out int keyNr) && keyNr is >= 72861000 and <= 72862000)
-            {
-                if (!Game1.player.hasQuest(key))
-                {
-                    candidates.Add(key);
-                }
-            }
-        }
-        
-        Log.Trace($"{candidates.Count} candidates for daily Quest");
-
-        if(candidates.Count == 0)
-        {
-            return null;
-        }
-
-        var rand = Game1.random.Next(candidates.Count);
-
-        Log.Trace($"chose {candidates[rand]}");
-        return GetQuestFromId(candidates[rand]);
-    }
-
-    private Quest GetRSVFishingQuest()
-    {
-        var quest = new FishingQuest();
-        quest.loadQuestInfo();
-
-        var possibleFish = Game1.currentSeason switch
-        {
-            "spring" => new[]
-            {
-                "Rafseazz.RSVCP_Cutthroat_Trout", "Rafseazz.RSVCP_Ridgeside_Bass", "Rafseazz.RSVCP_Ridge_Bluegill", "Rafseazz.RSVCP_Caped_Tree_Frog",
-                "Rafseazz.RSVCP_Pebble_Back_Crab", "Rafseazz.RSVCP_Harvester_Trout", "Rafseazz.RSVCP_Mountain_Redbelly_Dace", "Rafseazz.RSVCP_Mountain_Whitefish"
-            },
-            "summer" => new[]
-            {
-                "Rafseazz.RSVCP_Cutthroat_Trout", "Rafseazz.RSVCP_Ridgeside_Bass", "Rafseazz.RSVCP_Caped_Tree_Frog", "Rafseazz.RSVCP_Pebble_Back_Crab",
-                "Rafseazz.RSVCP_Skulpin_Fish", "Rafseazz.RSVCP_Mountain_Redbelly_Dace", "Rafseazz.RSVCP_Mountain_Whitefish"
-            },
-            "fall" => new[]
-            {
-                "Rafseazz.RSVCP_Cutthroat_Trout", "Rafseazz.RSVCP_Ridgeside_Bass", "Rafseazz.RSVCP_Ridge_Bluegill", "Rafseazz.RSVCP_Caped_Tree_Frog",
-                "Rafseazz.RSVCP_Pebble_Back_Crab", "Rafseazz.RSVCP_Skulpin_Fish", "Rafseazz.RSVCP_Harvester_Trout", "Rafseazz.RSVCP_Mountain_Redbelly_Dace",
-                "Rafseazz.RSVCP_Mountain_Whitefish"
-            },
-            "winter" => new[]
-            {
-                "Rafseazz.RSVCP_Ridgeside_Bass", "Rafseazz.RSVCP_Ridge_Bluegill", "Rafseazz.RSVCP_Skulpin_Fish", "Rafseazz.RSVCP_Harvester_Trout",
-                "Rafseazz.RSVCP_Mountain_Redbelly_Dace", "Rafseazz.RSVCP_Mountain_Whitefish"
-            },
-            _ => new[] { "132" }
-        };
-
-        var chosenFish = "(O)" + Game1.random.ChooseFrom(possibleFish);
-        quest.ItemId.Value = chosenFish;
-        if (!ItemRegistry.Exists(chosenFish))
-        {
-            quest.ItemId.Value = "(O)132";
-        }
-
-        var fishItem = ItemRegistry.Create(chosenFish);
-        quest.numberToFish.Value = (int)Math.Ceiling(200.0 / Math.Max(1, fishItem.salePrice())) + Game1.player.FishingLevel / 5;
-        quest.reward.Value = (int)(quest.numberToFish.Value + 1.5) * fishItem.salePrice();
-        quest.target.Value = "Carmen";
-        quest.parts.Clear();
-        quest.parts.Add(new DescriptionElement("Strings\\StringsFromCSFiles:Carmen.FishingQuest.Description", fishItem.DisplayName, quest.numberToFish.Value));
-        quest.dialogueparts.Clear();
-        quest.dialogueparts.Add(new DescriptionElement("Strings\\StringsFromCSFiles:Carmen.FishingQuest.HandInDialogue", fishItem.DisplayName));
-        quest.objective.Value = new DescriptionElement("Strings\\StringsFromCSFiles:FishingQuest.cs.13244", 0, quest.numberToFish.Value, fishItem.DisplayName);
-        quest.parts.Add(new DescriptionElement("Strings\\StringsFromCSFiles:FishingQuest.cs.13274", quest.reward.Value));
-        quest.parts.Add("Strings\\StringsFromCSFiles:FishingQuest.cs.13275");
-        quest.daysLeft.Value = 7;
-        quest.id.Value = "80000000";
-        return quest;
-    }
-    
-    private Quest? GetQuestFromId(string id)
-    {
-        Log.Trace($"Trying to load quest {id}");
-        Quest? quest;
-        try
-        {
-            quest = Quest.getQuestFromId(id);
-
-            if (quest is SlayMonsterQuest monsterQuest)
-            {
-                Dictionary<string, string> questData = Game1.temporaryContent.Load<Dictionary<string, string>>("Data\\Quests");
-
-                if (questData != null && questData.ContainsKey(id))
-                {
-                    var rawData = questData[id].Split('/');
-                    var conditionsSplit = rawData[4].Split(' ');
-
-                    monsterQuest.loadQuestInfo();
-                    monsterQuest.monster.Value.Name = conditionsSplit[0].Replace('_', ' ');
-                    monsterQuest.monsterName.Value = monsterQuest.monster.Value.Name;
-                    monsterQuest.numberToKill.Value = Convert.ToInt32(conditionsSplit[1]);
-                    monsterQuest.target.Value = conditionsSplit.Length > 2 ? conditionsSplit[2] : "null";
-                    monsterQuest.questType.Value = 4;
-                    if (rawData.Length > 9)
-                    {
-                        monsterQuest.targetMessage = rawData[9];
-                    }
-
-                    monsterQuest.moneyReward.Value = Convert.ToInt32(rawData[6]);
-                    monsterQuest.reward.Value = monsterQuest.moneyReward.Value;
-                    monsterQuest.rewardDescription.Value = rawData[6].Equals("-1") ? null : rawData[7];
-                    monsterQuest.parts.Clear();
-                    monsterQuest.dialogueparts.Clear();
-                    helper.Reflection.GetField<bool>(monsterQuest, "_loadedDescription").SetValue(true);
-                    return monsterQuest;
-                }
-            }
-        }
-        catch(Exception e)
-        {
-            Log.Error($"Failed parsing quest with id {id}");
-            Log.Error(e.Message);
-            Log.Error(e.StackTrace);
-            quest = null;
-        }
-			
-
-        return quest;
+        return Game1.random.NextBool()
+            ? AccessTools.Method(Type.GetType("RidgesideVillage.Questing.QuestFactory,RidgesideVillage"), "GetRandomHandCraftedQuest").Invoke(null, null) as Quest
+            : AccessTools.Method(Type.GetType("RidgesideVillage.Questing.QuestFactory,RidgesideVillage"), "GetFishingQuest").Invoke(null, null) as Quest;
     }
 }
 

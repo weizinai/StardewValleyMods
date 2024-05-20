d4878c770cdfbd943461a4bd3e6ff992d309884f
weizinai <1776061683@qq.com>
2024-05-20 22:56:03 +0800

    补全IGenericModConfigMenuApi

--- Common/Integrations/GenericModConfigMenu/IGenericModConfigMenuApi.cs
+++ Common/Integrations/GenericModConfigMenu/IGenericModConfigMenuApi.cs
@@ -1,5 +1,8 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Utilities;
+using StardewValley;
 
 namespace Common.Integrations;
 
@@ -23,6 +26,13 @@ public interface IGenericModConfigMenuApi
     /// <param name="mod">The mod's manifest.</param>
     /// <param name="text">The paragraph text to display.</param>
     public void AddParagraph(IManifest mod, Func<string> text);
+    
+    /// <summary>Add an image at the current position in the form.</summary>
+    /// <param name="mod">The mod's manifest.</param>
+    /// <param name="texture">The image texture to display.</param>
+    /// <param name="texturePixelArea">The pixel area within the texture to display, or <c>null</c> to show the entire image.</param>
+    /// <param name="scale">The zoom factor to apply to the image.</param>
+    public void AddImage(IManifest mod, Func<Texture2D> texture, Rectangle? texturePixelArea = null, int scale = Game1.pixelZoom);
 
     /// <summary>Add a boolean option at the current position in the form.</summary>
     /// <param name="mod">The mod's manifest.</param>
@@ -76,6 +86,15 @@ public interface IGenericModConfigMenuApi
     /// <param name="fieldId">The unique field ID for use with <see cref="OnFieldChanged" />, or <c>null</c> to auto-generate a randomized ID.</param>
     public void AddTextOption(IManifest mod, Func<string> getValue, Action<string> setValue, Func<string> name, Func<string>? tooltip = null,
         string[]? allowedValues = null, Func<string, string>? formatAllowedValue = null, string? fieldId = null);
+    
+    /// <summary>Add a key binding at the current position in the form.</summary>
+    /// <param name="mod">The mod's manifest.</param>
+    /// <param name="getValue">Get the current value from the mod config.</param>
+    /// <param name="setValue">Set a new value in the mod config.</param>
+    /// <param name="name">The label text to show in the form.</param>
+    /// <param name="tooltip">The tooltip text shown when the cursor hovers on the field, or <c>null</c> to disable the tooltip.</param>
+    /// <param name="fieldId">The unique field ID for use with <see cref="OnFieldChanged"/>, or <c>null</c> to auto-generate a randomized ID.</param>
+    public void AddKeybind(IManifest mod, Func<SButton> getValue, Action<SButton> setValue, Func<string> name, Func<string>? tooltip = null, string? fieldId = null);
 
     /// <summary>Add a key binding list at the current position in the form.</summary>
     /// <param name="mod">The mod's manifest.</param>
@@ -104,6 +123,30 @@ public interface IGenericModConfigMenuApi
     /// <param name="tooltip">The tooltip text shown when the cursor hovers on the link, or <c>null</c> to disable the tooltip.</param>
     public void AddPageLink(IManifest mod, string pageId, Func<string> text, Func<string>? tooltip = null);
 
+    /// <summary>Add an option at the current position in the form using custom rendering logic.</summary>
+    /// <param name="mod">The mod's manifest.</param>
+    /// <param name="name">The label text to show in the form.</param>
+    /// <param name="draw">Draw the option in the config UI. This is called with the sprite batch being rendered and the pixel position at which to start drawing.</param>
+    /// <param name="tooltip">The tooltip text shown when the cursor hovers on the field, or <c>null</c> to disable the tooltip.</param>
+    /// <param name="beforeMenuOpened">A callback raised just before the menu containing this option is opened.</param>
+    /// <param name="beforeSave">A callback raised before the form's current values are saved to the config (i.e. before the <c>save</c> callback passed to <see cref="Register"/>).</param>
+    /// <param name="afterSave">A callback raised after the form's current values are saved to the config (i.e. after the <c>save</c> callback passed to <see cref="Register"/>).</param>
+    /// <param name="beforeReset">A callback raised before the form is reset to its default values (i.e. before the <c>reset</c> callback passed to <see cref="Register"/>).</param>
+    /// <param name="afterReset">A callback raised after the form is reset to its default values (i.e. after the <c>reset</c> callback passed to <see cref="Register"/>).</param>
+    /// <param name="beforeMenuClosed">A callback raised just before the menu containing this option is closed.</param>
+    /// <param name="height">The pixel height to allocate for the option in the form, or <c>null</c> for a standard input-sized option. This is called and cached each time the form is opened.</param>
+    /// <param name="fieldId">The unique field ID for use with <see cref="OnFieldChanged"/>, or <c>null</c> to auto-generate a randomized ID.</param>
+    /// <remarks>The custom logic represented by the callback parameters is responsible for managing its own state if needed. For example, you can store state in a static field or use closures to use a state variable.</remarks>
+    public void AddComplexOption(IManifest mod, Func<string> name, Action<SpriteBatch, Vector2> draw, 
+        Func<string>? tooltip = null, Action? beforeMenuOpened = null, Action? beforeSave = null, Action? afterSave = null, 
+        Action? beforeReset = null, Action? afterReset = null, Action? beforeMenuClosed = null, Func<int>? height = null, string? fieldId = null);
+    
+    /// <summary>Set whether the options registered after this point can only be edited from the title screen.</summary>
+    /// <param name="mod">The mod's manifest.</param>
+    /// <param name="titleScreenOnly">Whether the options can only be edited from the title screen.</param>
+    /// <remarks>This lets you have different values per-field. Most mods should just set it once in <see cref="Register"/>.</remarks>
+    public void SetTitleScreenOnlyForNextOptions(IManifest mod, bool titleScreenOnly);
+
     /// <summary>Register a method to notify when any option registered by this mod is edited through the config UI.</summary>
     /// <param name="mod">The mod's manifest.</param>
     /// <param name="onChange">The method to call with the option's unique field ID and new value.</param>
@@ -113,6 +156,12 @@ public interface IGenericModConfigMenuApi
     /// <summary>Open the config UI for a specific mod.</summary>
     /// <param name="mod">The mod's manifest.</param>
     public void OpenModMenu(IManifest mod);
+    
+    /// <summary>Get the currently-displayed mod config menu, if any.</summary>
+    /// <param name="mod">The manifest of the mod whose config menu is being shown, or <c>null</c> if not applicable.</param>
+    /// <param name="page">The page ID being shown for the current config menu, or <c>null</c> if not applicable. This may be <c>null</c> even if a mod config menu is shown (e.g. because the mod doesn't have pages).</param>
+    /// <returns>Returns whether a mod config menu is being shown.</returns>
+    public bool TryGetCurrentMenu(out IManifest mod, out string page);
 
     /// <summary>Remove a mod from the config UI and delete all its options and pages.</summary>
     /// <param name="mod">The mod's manifest.</param>

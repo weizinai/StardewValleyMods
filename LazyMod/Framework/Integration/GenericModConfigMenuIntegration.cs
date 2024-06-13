97d9aad567fed5e9f19e5372cb4496555e02e83f
weizinai <1776061683@qq.com>
2024-06-13 18:20:13 +0800

    创建GenericModConfigMenu<TConfig>的部分类，以实现LazyMod中的重复逻辑

---
+++ LazyMod/Framework/Integration/GenericModConfigMenuIntegration.cs
@@ -0,0 +1,30 @@
+using LazyMod;
+using LazyMod.Framework.Config;
+
+// ReSharper disable once CheckNamespace
+namespace Common.Integrations;
+
+/// <summary>Handles the other logic for integrating with the Generic Mod Config Menu mod.</summary>
+/// <typeparam name="TConfig">The mod config type.</typeparam>
+internal partial class GenericModConfigMenuIntegration<TConfig> where TConfig : new()
+{
+    public GenericModConfigMenuIntegration<TConfig> AddBaseAutomationConfig(Func<TConfig, BaseAutomationConfig> get, Func<string> text, Func<string> tooltip, int minRange = 0)
+    {
+        AddSectionTitle(text, tooltip);
+            AddBoolOption(
+                config => get(config).IsEnable,
+                (config, value) => get(config).IsEnable = value,
+                I18n.Config_Enable_Name
+            );
+            AddNumberOption(
+                config => get(config).Range,
+                (config, value) => get(config).Range = value,
+                I18n.Config_Range_Name,
+                null,
+                minRange,
+                3
+            );
+
+        return this;
+    }
+}

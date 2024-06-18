a509fbb5cf7b529e5043f695538d2e9dcd3ede04
weizinai <1776061683@qq.com>
2024-06-19 01:06:09 +0800

    重新排序

--- SomeMultiplayerFeature/Framework/ModConfig.cs
+++ SomeMultiplayerFeature/Framework/ModConfig.cs
@@ -5,11 +5,22 @@ namespace SomeMultiplayerFeature.Framework;
 
 public class ModConfig
 {
+    // 访问商店信息
     public bool ShowAccessShopInfo { get; set; } = true;
-    public bool EnableModLimit { get; set; } = true;
+    
+    // 踢出延迟玩家
     public bool EnableKickDelayedPlayer { get; set; } = true;
-    public bool ShowPlayerCount { get; set; } = true;
-    public bool EnableKickUnreadyPlayer { get; set; } = true;
-    public KeybindList KickUnreadyPlayerKey { get; set; } = new(SButton.F2);
+    
+    // 模组限制
+    public bool EnableModLimit { get; set; } = true;
+    
+    // 禁用金钱作弊
     public bool BanMoneyCheat { get; set; } = true;
+    
+    // 显示玩家数量
+    public bool ShowPlayerCount { get; set; } = true;
+    
+    // 踢出未准备玩家
+    public bool EnableKickUnreadyPlayer { get; set; } = true;
+    public KeybindList KickUnreadyPlayerKey { get; set; } = new(SButton.F3);
 }

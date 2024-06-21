9ce50b31b5b502746544c18ee0024a06c328d962
weizinai <1776061683@qq.com>
2024-06-22 01:30:23 +0800

    让名字标签居中显示

--- BetterCabin/Framework/CabinOwnerNameBox.cs
+++ BetterCabin/Framework/CabinOwnerNameBox.cs
@@ -16,8 +16,15 @@ internal class CabinOwnerNameBox
     private SpriteFont Font => Game1.smallFont;
     private string Name => cabin.owner.displayName;
     private Point Size => Font.MeasureString(Name).ToPoint() + new Point(32, 32);
-    private Point Position => new(building.tileX.Value * 64 - Game1.viewport.X + offset.X, 
-        building.tileY.Value * 64 - Game1.viewport.Y + offset.Y);
+
+    private Point Position
+    {
+        get
+        {
+            var buildingPosition = new Point(building.tileX.Value * 64 - Game1.viewport.X, building.tileY.Value * 64 - Game1.viewport.Y);
+            return new Point(buildingPosition.X - Size.X / 2 + offset.X, buildingPosition.Y - Size.Y / 2 + offset.Y);
+        }
+    }
 
     public CabinOwnerNameBox(Building building, Cabin cabin, Point offset)
     {
@@ -29,7 +36,7 @@ internal class CabinOwnerNameBox
     public void Draw(SpriteBatch spriteBatch)
     {
         IClickableMenu.drawTextureBox(spriteBatch, Position.X, Position.Y, Size.X, Size.Y, Color.White);
-        Utility.drawTextWithShadow(spriteBatch, Name, Font, new Vector2(Position.X + 16, Position.Y + 16), 
+        Utility.drawTextWithShadow(spriteBatch, Name, Font, new Vector2(Position.X + 16, Position.Y + 16),
             Game1.player.Equals(cabin.owner) ? Color.Red : Color.Black, 1f, 1f);
     }
 }

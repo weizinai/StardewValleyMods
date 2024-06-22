757f7c0f8f564bdd3219c56425d3c1a3bb727813
weizinai <1776061683@qq.com>
2024-06-22 15:13:52 +0800

    抽象出Box类，以变添加更多小屋标签

---
+++ BetterCabin/Framework/UI/Box.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Locations;
+using StardewValley.Menus;
+
+namespace BetterCabin.Framework.UI;
+
+internal abstract class Box
+{
+    protected readonly Building building;
+    protected readonly Cabin cabin;
+    protected readonly ModConfig config;
+    
+    protected SpriteFont Font => Game1.smallFont;
+    protected abstract Color TextColor { get; }
+    protected abstract string Text { get; }
+    protected Point Size => Font.MeasureString(Text).ToPoint() + new Point(32, 32);
+    protected Point Position
+    {
+        get
+        {
+            var buildingPosition = new Point(building.tileX.Value * 64 - Game1.viewport.X, building.tileY.Value * 64 - Game1.viewport.Y);
+            return new Point(buildingPosition.X - Size.X / 2 + Offset.X, buildingPosition.Y - Size.Y / 2 + Offset.Y);
+        }
+    }
+    protected abstract Point Offset { get; }
+
+    protected Box(Building building, Cabin cabin, ModConfig config)
+    {
+        this.building = building;
+        this.cabin = cabin;
+        this.config = config;
+    }
+    
+    public void Draw(SpriteBatch spriteBatch)
+    {
+        IClickableMenu.drawTextureBox(spriteBatch, Position.X, Position.Y, Size.X, Size.Y, Color.White);
+        Utility.drawTextWithShadow(spriteBatch, Text, Font, new Vector2(Position.X + 16, Position.Y + 16), TextColor, 1f, 1f);
+    }
+}
---
+++ BetterCabin/Framework/UI/CabinOwnerNameBox.cs
@@ -0,0 +1,29 @@
+using Microsoft.Xna.Framework;
+using StardewValley;
+using StardewValley.Buildings;
+using StardewValley.Locations;
+
+namespace BetterCabin.Framework.UI;
+
+internal class CabinOwnerNameBox : Box
+{
+    protected override Color TextColor
+    {
+        get
+        {
+            if (Game1.player.Equals(cabin.owner))
+            {
+                return config.OwnerColor;
+            }
+
+            return Game1.player.team.playerIsOnline(cabin.owner.UniqueMultiplayerID) ? config.OnlineFarmerColor : config.OfflineFarmerColor;
+        }
+    }
+    protected override string Text => cabin.owner.displayName;
+    protected override Point Offset => new(config.XOffset, config.YOffset);
+
+    public CabinOwnerNameBox(Building building, Cabin cabin, ModConfig config) 
+        : base(building, cabin, config)
+    {
+    }
+}

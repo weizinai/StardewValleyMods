a2bd096ab22fc90a0b4a1de004ebe47390dd2527
weizinai <1776061683@qq.com>
2024-06-24 18:27:28 +0800

    在GMCM中为选项更新提示

--- MultiplayerModLimit/Framework/GenericModConfigMenuIntegrationForMultiplayerModLimit.cs
+++ MultiplayerModLimit/Framework/GenericModConfigMenuIntegrationForMultiplayerModLimit.cs
@@ -29,21 +29,23 @@ internal class GenericModConfigMenuIntegrationForMultiplayerModLimit
             .AddNumberOption(
                 config => config.KickPlayerDelayTime,
                 (config, value) => config.KickPlayerDelayTime = value,
-                I18n.Config_KickPlayerDelayTime_Name
+                I18n.Config_KickPlayerDelayTime_Name,
+                I18n.Config_KickPlayerDelayTIme_Tooltip
             )
             .AddSectionTitle(I18n.Config_LimitSettingTitle_Name)
             // 要求SMAPI
             .AddBoolOption(
                 config => config.RequireSMAPI,
                 (config, value) => config.RequireSMAPI = value,
-                I18n.Config_RequireSMAPI_Name
+                I18n.Config_RequireSMAPI_Name,
+                I18n.Config_RequireSMAPI_Tooltip
             )
             // 限制模式
             .AddTextOption(
                 config => config.LimitMode.ToString(),
                 (config, value) => config.LimitMode = Enum.Parse<LimitMode>(value),
                 I18n.Config_LimitMode_Name,
-                null,
+                I18n.Config_LimitMode_Tooltip,
                 new[] { "WhiteListMode", "BlackListMode" },
                 value => value switch
                 {
@@ -52,26 +54,28 @@ internal class GenericModConfigMenuIntegrationForMultiplayerModLimit
                     _ => value
                 }
             )
+            .AddSectionTitle(I18n.Config_ModListSelectedTitle_Name)
+            .AddParagraph(I18n.Config_ModListSelected_Paragraph)
             // 选择的模组列表
             .AddTextOption(
                 config => config.AllowedModListSelected,
                 (config, value) => config.AllowedModListSelected = value,
                 I18n.Config_AllowedModListSelected_Name,
-                null,
+                I18n.Config_AllowedModListSelected_Tooltip,
                 this.configMenu.GetConfig().AllowedModList.Keys.ToArray()
             )
             .AddTextOption(
                 config => config.RequiredModListSelected,
                 (config, value) => config.RequiredModListSelected = value,
                 I18n.Config_RequiredModListSelected_Name,
-                null,
+                I18n.Config_RequiredModListSelected_Tooltip,
                 this.configMenu.GetConfig().RequiredModList.Keys.ToArray()
             )
             .AddTextOption(
                 config => config.BannedModListSelected,
                 (config, value) => config.BannedModListSelected = value,
                 I18n.Config_BannedModListSelected_Name,
-                null,
+                I18n.Config_BannedModListSelected_Tooltip,
                 this.configMenu.GetConfig().BannedModList.Keys.ToArray()
             )
             ;

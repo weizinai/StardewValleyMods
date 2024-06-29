65baa729d981732b47c38a9e9bf587529e88b648
weizinai <1776061683@qq.com>
2024-06-29 15:23:47 +0800

    创建RSVReflection类，以方便的获取RSV中的方法进行调用

---
+++ ActiveMenuAnywhere/Framework/RSVReflection.cs
@@ -0,0 +1,21 @@
+using System.Reflection;
+
+namespace weizinai.StardewValleyMod.ActiveMenuAnywhere.Framework;
+
+internal static class RSVReflection
+{
+    public static Type GetRSVType(string typeName)
+    {
+        var type = Type.GetType($"{typeName}, RidgesideVillage");
+        if (type is null) throw new ArgumentException($"Could not find type {typeName} in RidgesideVillage assembly.");
+        return type;
+    }
+    
+    public static MethodInfo GetRSVPrivateStaticMethod(string typeName, string methodName)
+    {
+        var type = GetRSVType(typeName);
+        var method = type.GetMethod(methodName, BindingFlags.NonPublic | BindingFlags.Static);
+        if (method is null) throw new ArgumentException($"Could not find method {methodName} in type {typeName}.");
+        return method;
+    }
+}

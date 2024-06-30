e4028ea7940a18b44072ba1799284612bdc5300f
weizinai <1776061683@qq.com>
2024-06-30 22:20:36 +0800

    修改获取玩家的方法由foreach改为linq

--- SpectatorMode/Handler/SpectatePlayerHandler.cs
+++ SpectatorMode/Handler/SpectatePlayerHandler.cs
@@ -20,9 +20,9 @@ internal class SpectatePlayerHandler : BaseHandler
 
         if (this.Config.SpectatePlayerKey.JustPressed())
         {
-            var players = new List<KeyValuePair<string, string>>();
-            foreach (var (_, farmer) in Game1.otherFarmers) players.Add(new KeyValuePair<string, string>(farmer.Name, farmer.displayName));
-            Game1.currentLocation.ShowPagedResponses(I18n.UI_SpectatePlayer_Title(), players, 
+            var players = Game1.otherFarmers.Select(x => new KeyValuePair<string, string>(x.Value.Name, x.Value.displayName));
+            
+            Game1.currentLocation.ShowPagedResponses(I18n.UI_SpectatePlayer_Title(), players.ToList(), 
                 value => SpectatorHelper.TrySpectateFarmer(value), false, true, 10);
         }
     }

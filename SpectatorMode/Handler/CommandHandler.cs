47f0042ded328db2c1cecc3b7697bff97e775dc2
weizinai <1776061683@qq.com>
2024-06-30 22:02:56 +0800

    修复SMAPI控制台信息显示不对的问题

--- SpectatorMode/Handler/CommandHandler.cs
+++ SpectatorMode/Handler/CommandHandler.cs
@@ -19,20 +19,18 @@ internal class CommandHandler : BaseHandler
     {
         var locationName = args[0];
 
-        if (SpectatorHelper.TrySpectateLocation(locationName))
-            Log.Info(I18n.UI_SpectateLocation_Fail(locationName));
-        else
-            Log.Info(I18n.UI_SpectateLocation_Fail(locationName));
+        Log.Info(SpectatorHelper.TrySpectateLocation(locationName)
+            ? I18n.UI_SpectateLocation_Success(locationName)
+            : I18n.UI_SpectateLocation_Fail(locationName));
     }
 
     // 旁观玩家
     private void SpectateFarmer(string command, string[] args)
     {
         var playerName = args[0];
-        
-        if (SpectatorHelper.TrySpectateFarmer(playerName))
-            Log.Info(I18n.UI_SpectatePlayer_Success(playerName));
-        else
-            Log.Info(I18n.UI_SpectatePlayer_Success(playerName));
+
+        Log.Info(SpectatorHelper.TrySpectateFarmer(playerName)
+            ? I18n.UI_SpectatePlayer_Success(playerName)
+            : I18n.UI_SpectatePlayer_Fail(playerName));
     }
 }

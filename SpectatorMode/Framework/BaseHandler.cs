e7a452226efa922d8892358909ea1be665d62d92
weizinai <1776061683@qq.com>
2024-06-30 17:12:16 +0800

    添加BaseHandler类和IHandler接口，以将功能拆分成单个文件

---
+++ SpectatorMode/Framework/BaseHandler.cs
@@ -0,0 +1,16 @@
+using StardewModdingAPI;
+
+namespace weizinai.StardewValleyMod.SpectatorMode.Framework;
+
+internal abstract class BaseHandler : IHandler
+{
+    protected readonly IModHelper Helper;
+    protected readonly ModConfig Config;
+
+    protected BaseHandler(IModHelper helper, ModConfig config)
+    {
+        this.Helper = helper;
+        this.Config = config;
+    }
+    public abstract void Init();
+}
---
+++ SpectatorMode/Framework/IHandler.cs
@@ -0,0 +1,6 @@
+namespace weizinai.StardewValleyMod.SpectatorMode.Framework;
+
+internal interface IHandler
+{
+    public void Init();
+}

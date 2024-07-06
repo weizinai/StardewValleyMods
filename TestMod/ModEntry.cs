c38427ae373c27d9a934803d74f8b7987664bf0b
weizinai <1776061683@qq.com>
2024-07-06 16:15:17 +0800

    添加修改存档功能

--- TestMod/ModEntry.cs
+++ TestMod/ModEntry.cs
@@ -9,17 +9,32 @@ namespace TestMod;
 public class ModEntry : Mod
 {
     private readonly KeybindList testKet = new(SButton.O);
-    
+    private const string SavePath = @"C:\Projects\StardewValleyMods\TestMod\assets\联机测试_374061236.xml";
+
     public override void Entry(IModHelper helper)
     {
         Log.Init(this.Monitor);
-        
+
         helper.Events.Input.ButtonsChanged += this.OnButtonChanged;
     }
 
     private void OnButtonChanged(object? sender, ButtonsChangedEventArgs e)
     {
         if (this.testKet.JustPressed())
-            Log.Info(Game1.player.slotName);
+        {
+            SaveGame saveGame;
+            using (var fileStream = new FileStream(SavePath, FileMode.Open))
+            {
+                saveGame = (SaveGame)SaveGame.serializer.Deserialize(fileStream)!;
+                Log.Info(saveGame.player.UniqueMultiplayerID.ToString());
+            }
+
+            (saveGame.player, saveGame.farmhands[0]) = (saveGame.farmhands[0], saveGame.player);
+
+            using (var streamWriter = new StreamWriter(SavePath))
+            {
+                SaveGame.serializer.Serialize(streamWriter, saveGame);
+            }
+        }
     }
 }

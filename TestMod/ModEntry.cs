9b9e09ee72e09920f8882641e5d152fc9abad8c7
weizinai <1776061683@qq.com>
2024-07-06 14:47:16 +0800

    新建TestMod项目

---
+++ TestMod/ModEntry.cs
@@ -0,0 +1,25 @@
+﻿using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
+using StardewValley;
+using weizinai.StardewValleyMod.Common.Log;
+
+namespace TestMod;
+
+public class ModEntry : Mod
+{
+    private readonly KeybindList testKet = new(SButton.O);
+    
+    public override void Entry(IModHelper helper)
+    {
+        Log.Init(this.Monitor);
+        
+        helper.Events.Input.ButtonsChanged += this.OnButtonChanged;
+    }
+
+    private void OnButtonChanged(object? sender, ButtonsChangedEventArgs e)
+    {
+        if (this.testKet.JustPressed())
+            Log.Info(Game1.player.slotName);
+    }
+}

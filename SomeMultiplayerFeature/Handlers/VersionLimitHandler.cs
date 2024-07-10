a435b0fb4b7966837af8ad0cf726cd703ed4c522
weizinai <1776061683@qq.com>
2024-07-11 01:31:38 +0800

    重写版本限制功能的代码

--- SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
+++ SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
@@ -9,74 +9,53 @@ namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Handlers;
 
 internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
 {
-    private const string ModKey = "SomeMultiplayerFeature_Version";
-
-    private int actionCount;
+    private const string VersionLimitKey = "SomeMultiplayerFeature_Version";
+    private const string TargetVersion = "0.9.0";
 
     public VersionLimitHandler(IModHelper helper, ModConfig config)
         : base(helper, config) { }
 
     public override void Init()
     {
-        this.Helper.Events.GameLoop.TimeChanged += this.OnTimeChanged;
         this.Helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         this.Helper.Events.Multiplayer.PeerConnected += this.OnPeerConnected;
         this.Helper.Events.Multiplayer.ModMessageReceived += this.OnModMessageReceived;
     }
 
-    private void OnTimeChanged(object? sender, TimeChangedEventArgs e)
-    {
-        // 如果该功能未启用，则返回
-        if (!this.Config.VersionLimit) return;
-
-        // 如果当前没有玩家在线或者当前玩家不是主机端，则返回
-        if (!Context.HasRemotePlayers || !Context.IsMainPlayer) return;
-
-        if (this.actionCount > 0)
-        {
-            foreach (var (id, farmer) in Game1.otherFarmers)
-            {
-                if (!farmer.modData.ContainsKey(ModKey) || farmer.modData[ModKey] != "0.9.0")
-                {
-                    this.Helper.Multiplayer.SendMessage($"{farmer.Name}已被踢出，因为其SomeMultiplayerFeature模组不是最新版。", "VersionLimit",
-                        new[] { "weizinai.SomeMultiplayerFeature" }, new[] { id });
-                    Game1.server.kick(id);
-                }
-            }
-
-            this.actionCount--;
-        }
-    }
-
-    // 当载入存档时，向玩家写入版本验证信息
     private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
     {
-        // 如果当前玩家是主机端，则返回
-        if (Context.IsMainPlayer) return;
+        if (Game1.IsServer) return;
 
         var farmer = Game1.player;
-        if (farmer.modData.ContainsKey(ModKey))
-            farmer.modData[ModKey] = "0.9.0";
-        else
-            farmer.modData.Add(ModKey, "0.9.0");
+        if (!farmer.modData.TryAdd(VersionLimitKey, TargetVersion))
+            farmer.modData[VersionLimitKey] = TargetVersion;
     }
 
     private void OnPeerConnected(object? sender, PeerConnectedEventArgs e)
     {
-        // 如果当前不是联机模式或者当前玩家不是主机端，则返回
-        if (!Context.IsMultiplayer || !Context.IsMainPlayer) return;
+        if (Game1.IsClient) return;
 
-        this.actionCount++;
+        DelayedAction.functionAfterDelay(() =>
+        {
+            if (!this.Config.VersionLimit) return;
+
+            var id = e.Peer.PlayerID;
+            var farmer = Game1.getFarmer(id);
+            if (!farmer.modData.ContainsKey(VersionLimitKey) || farmer.modData[VersionLimitKey] != TargetVersion)
+            {
+                this.Helper.Multiplayer.SendMessage("", "VersionLimit", new[] { "weizinai.SomeMultiplayerFeature" }, new[] { id });
+                Game1.server.kick(id);
+            }
+        }, 5000);
     }
 
     private void OnModMessageReceived(object? sender, ModMessageReceivedEventArgs e)
     {
-        if (Context.IsMainPlayer) return;
+        if (Game1.IsServer) return;
 
         if (e is { Type: "VersionLimit", FromModID: "weizinai.SomeMultiplayerFeature" })
         {
-            var message = e.ReadAs<string>();
-            Log.Alert(message);
+            Log.Alert("你的<SomeMultiplayerFeature>模组不是最新版，已被踢出。");
         }
     }
 }

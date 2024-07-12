ba0a40c3772f9119d37f05ded8837b27b075a992
weizinai <1776061683@qq.com>
2024-07-12 19:03:37 +0800

    将客机版本验证不通过的消息移至聊天框

--- SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
+++ SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
@@ -2,14 +2,13 @@ using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
 using weizinai.StardewValleyMod.Common.Handler;
-using weizinai.StardewValleyMod.Common.Log;
 using weizinai.StardewValleyMod.SomeMultiplayerFeature.Framework;
 
 namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Handlers;
 
 internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
 {
-    private const string VersionLimitKey = "SomeMultiplayerFeature_Version";
+    private const string VersionLimitKey = "weizinai.SomeMultiplayerFeature_Version";
     private const string TargetVersion = "0.10.0";
 
     public VersionLimitHandler(IModHelper helper, ModConfig config)
@@ -19,51 +18,34 @@ internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
     {
         this.Helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         this.Helper.Events.Multiplayer.PeerConnected += this.OnPeerConnected;
-        this.Helper.Events.Multiplayer.ModMessageReceived += this.OnModMessageReceived;
     }
 
     public override void Clear()
     {
         this.Helper.Events.GameLoop.SaveLoaded -= this.OnSaveLoaded;
         this.Helper.Events.Multiplayer.PeerConnected -= this.OnPeerConnected;
-        this.Helper.Events.Multiplayer.ModMessageReceived -= this.OnModMessageReceived;
     }
 
     private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
     {
-        if (Game1.IsServer) return;
-
-        var farmer = Game1.player;
-        if (!farmer.modData.TryAdd(VersionLimitKey, TargetVersion))
-            farmer.modData[VersionLimitKey] = TargetVersion;
+        if (Game1.IsClient) Game1.player.modData[VersionLimitKey] = TargetVersion;
     }
 
     private void OnPeerConnected(object? sender, PeerConnectedEventArgs e)
     {
-        if (Game1.IsClient) return;
-
-        DelayedAction.functionAfterDelay(() =>
+        if (this.Config.VersionLimit && Game1.IsServer)
         {
-            if (!this.Config.VersionLimit) return;
-
-            var id = e.Peer.PlayerID;
-            var farmer = Game1.getFarmer(id);
-            if (!farmer.modData.ContainsKey(VersionLimitKey) || farmer.modData[VersionLimitKey] != TargetVersion)
+            DelayedAction.functionAfterDelay(() =>
             {
-                this.Helper.Multiplayer.SendMessage("", "VersionLimit", new[] { "weizinai.SomeMultiplayerFeature" }, new[] { id });
-                Game1.chatBox.addInfoMessage($"{farmer.Name}的<SomeMultiplayerFeature>模组不是最新版，已被踢出。");
-                Game1.server.kick(id);
-            }
-        }, 5000);
-    }
-
-    private void OnModMessageReceived(object? sender, ModMessageReceivedEventArgs e)
-    {
-        if (Game1.IsServer) return;
-
-        if (e is { Type: "VersionLimit", FromModID: "weizinai.SomeMultiplayerFeature" })
-        {
-            Log.Alert("你的<SomeMultiplayerFeature>模组不是最新版，已被踢出。");
+                var id = e.Peer.PlayerID;
+                var farmer = Game1.getFarmer(id);
+                if (!farmer.modData.ContainsKey(VersionLimitKey) || farmer.modData[VersionLimitKey] != TargetVersion)
+                {
+                    Game1.chatBox.addInfoMessage($"{farmer.Name}的<SomeMultiplayerFeature>模组不是最新版，将被踢出。");
+                    Game1.Multiplayer.sendChatMessage(LocalizedContentManager.CurrentLanguageCode, "你的<SomeMultiplayerFeature>模组不是最新版，将被踢出。", id);
+                    Game1.server.kick(id);
+                }
+            }, 5000);
         }
     }
 }

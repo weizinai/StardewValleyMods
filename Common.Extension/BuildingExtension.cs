00bb600a358d79c841953a986ac717eb84fd13e8
weizinai <1776061683@qq.com>
2024-07-13 17:50:06 +0800

    添加IsCabinWithOwner方法，简化判断建筑是否为有主人的小屋

--- Common.Extension/BuildingExtension.cs
+++ Common.Extension/BuildingExtension.cs
@@ -15,4 +15,16 @@ internal static class BuildingExtension
         cabin = null;
         return false;
     }
+
+    public static bool IsCabinWithOwner(this Building building, out Cabin? cabin)
+    {
+        if (building.IsCabin(out var value))
+        {
+            cabin = value;
+            return !value!.owner.isUnclaimedFarmhand;
+        }
+
+        cabin = null;
+        return false;
+    }
 }

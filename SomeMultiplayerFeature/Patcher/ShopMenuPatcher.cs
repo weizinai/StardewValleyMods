ec467b19dbf12c4b43da27986576f8cb3f61e5d3
weizinai <1776061683@qq.com>
2024-07-20 01:50:22 +0800

    基于新逻辑重新实现代码

--- SomeMultiplayerFeature/Patcher/ShopMenuPatcher.cs
+++ SomeMultiplayerFeature/Patcher/ShopMenuPatcher.cs
@@ -1,9 +1,9 @@
-using System.Text.Json;
 using HarmonyLib;
 using StardewValley;
 using StardewValley.Menus;
 using weizinai.StardewValleyMod.Common.Log;
 using weizinai.StardewValleyMod.Common.Patcher;
+using weizinai.StardewValleyMod.SomeMultiplayerFeature.Framework;
 using weizinai.StardewValleyMod.SomeMultiplayerFeature.Handlers;
 
 namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Patcher;
@@ -22,20 +22,21 @@ internal class ShopMenuPatcher : BasePatcher
     // 购物限制
     private static bool TryToPurchaseItemPrefix(ISalable item, ref int stockToBuy, ShopMenu __instance)
     {
-        if (Game1.IsServer) return true;
-
-        if (__instance.itemPriceAndStock[item].TradeItem != null) return true;
+        if (!CheckSpendLimitEnable(__instance.itemPriceAndStock[item])) return true;
 
         var player = Game1.player;
-
-        if (!TryGetPlayerLimit(player, out var limit, out var amount)) return true;
-
+        SpendLimitHelper.TryGetFarmerSpendLimit(player.Name, out var limit);
+        var amount = int.Parse(player.modData[SpendLimitHandler.SpendAmountKey]);
         var availableMoney = limit - amount;
-
-        if (availableMoney < item.salePrice() * stockToBuy)
+        var totalPrice = item.salePrice() * stockToBuy;
+        if (availableMoney < totalPrice)
         {
-            Game1.drawObjectDialogue(
-                $"你今日的消费金额为{amount}，总消费额度为{limit}，可用额度为{availableMoney}。购买{stockToBuy}个{item.DisplayName}需要{item.salePrice() * stockToBuy}金币，超过你的可用额度{availableMoney}。因此你的购物行为被禁止。");
+            var dialogues = new List<string>()
+            {
+                $"当日消费：{amount}金|可用额度：{availableMoney}金|总额度：{limit}金",
+                $"购买{stockToBuy}个{item.DisplayName}需要{totalPrice}金，超过可用额度{totalPrice - availableMoney}金"
+            };
+            Game1.drawObjectDialogue(dialogues);
             stockToBuy = 0;
             return false;
         }
@@ -49,21 +50,10 @@ internal class ShopMenuPatcher : BasePatcher
         if (stockToBuy > 0) MultiplayerLog.NoIconHUDMessage($"{Game1.player.Name}购买了 {stockToBuy} 个{item.DisplayName}", 500);
     }
 
-    private static bool TryGetPlayerLimit(Farmer player, out int limit, out int amount)
+    private static bool CheckSpendLimitEnable(ItemStockInformation itemStockInformation)
     {
-        Game1.MasterPlayer.modData.TryGetValue(SpendLimitHandler.SpendLimitDataKey, out var rawLimit);
-        player.modData.TryGetValue(SpendLimitHandler.SpendAmountKey, out var rawAmount);
-
-        if (rawLimit == null || rawAmount == null)
-        {
-            limit = 0;
-            amount = 0;
-            return false;
-        }
-
-        limit = JsonSerializer.Deserialize<Dictionary<string, int>>(rawLimit)![player.Name];
-        amount = int.Parse(rawAmount);
-
-        return true;
+        return !Game1.IsServer &&
+               Game1.MasterPlayer.modData.ContainsKey(SpendLimitHandler.SpendLimitKey) &&
+               itemStockInformation.TradeItem == null;
     }
 }

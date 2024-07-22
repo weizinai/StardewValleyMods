148f8e9e39316c4c804a81c967e0fa2422ef19ff
weizinai <1776061683@qq.com>
2024-07-22 15:43:30 +0800

    修复花钱限制和购物限制处理不正确的问题

--- SomeMultiplayerFeature/Patcher/ShopMenuPatcher.cs
+++ SomeMultiplayerFeature/Patcher/ShopMenuPatcher.cs
@@ -20,15 +20,13 @@ internal class ShopMenuPatcher : BasePatcher
     }
 
     // 购物限制
-    private static bool TryToPurchaseItemPrefix(ISalable item, ref int stockToBuy, ShopMenu __instance)
+    private static bool TryToPurchaseItemPrefix(ISalable item, ref int stockToBuy, ShopMenu __instance, ref bool __state)
     {
-        if (!CheckSpendLimitEnable(__instance.itemPriceAndStock[item])) return true;
+        if (!CheckSpendLimitEnable(__instance, item)) return true;
 
-        var player = Game1.player;
-        SpendLimitHelper.TryGetFarmerSpendLimit(player.Name, out var limit);
-        var amount = int.Parse(player.modData[SpendLimitHandler.SpendAmountKey]);
-        var availableMoney = limit - amount;
-        var totalPrice = item.salePrice() * stockToBuy;
+        GetFarmerSpendData(out var amount, out var limit, out var availableMoney);
+        var stockInfo = __instance.itemPriceAndStock[item];
+        var totalPrice = stockInfo.Price * stockToBuy;
         if (availableMoney < totalPrice)
         {
             var dialogues = new List<string>
@@ -41,19 +39,52 @@ internal class ShopMenuPatcher : BasePatcher
             return false;
         }
 
-        player.modData[SpendLimitHandler.SpendAmountKey] = (amount + item.salePrice() * stockToBuy).ToString();
+        GetTradeItemData(stockInfo, out var tradeItem, out var tradeItemCount);
+
+        var player = Game1.player;
+        if (player.Money >= totalPrice &&
+            (tradeItem == null || __instance.HasTradeItem(tradeItem, tradeItemCount * stockToBuy)))
+        {
+            player.modData[SpendLimitHandler.SpendAmountKey] = (amount + totalPrice).ToString();
+            __state = true;
+        }
+
         return true;
     }
 
-    private static void TryToPurchaseItemPostfix(ISalable item, int stockToBuy, ShopMenu __instance)
+    private static void TryToPurchaseItemPostfix(ISalable item, int stockToBuy, bool __state)
     {
-        if (stockToBuy > 0 && __instance.heldItem != null) MultiplayerLog.NoIconHUDMessage($"{Game1.player.Name}购买了 {stockToBuy} 个{item.DisplayName}", 500);
+        if (__state) MultiplayerLog.NoIconHUDMessage($"{Game1.player.Name}购买了 {stockToBuy} 个{item.DisplayName}", 500);
     }
 
-    private static bool CheckSpendLimitEnable(ItemStockInformation itemStockInformation)
+    private static bool CheckSpendLimitEnable(ShopMenu menu, ISalable item)
     {
-        return !Game1.IsServer &&
-               Game1.MasterPlayer.modData.ContainsKey(SpendLimitHandler.SpendLimitKey) &&
-               itemStockInformation.TradeItem == null;
+        return Game1.IsClient &&                                                          // 客户端
+               Game1.MasterPlayer.modData.ContainsKey(SpendLimitHandler.SpendLimitKey) && // 主机开启花钱限制功能
+               menu.currency == 0 &&                                                      // 商店货币为金币
+               (menu.heldItem == null || menu.heldItem.canStackWith(item)) &&             // 当前未持有物品或者持有的物品能与要购买的物品堆叠
+               Game1.player.couldInventoryAcceptThisItem(item as Item);                   // 玩家有足够的空间容纳要购买的物品
+    }
+
+    private static void GetFarmerSpendData(out int amount, out int limit, out int availableMoney)
+    {
+        var player = Game1.player;
+        SpendLimitHelper.TryGetFarmerSpendLimit(player.Name, out limit);
+        amount = int.Parse(player.modData[SpendLimitHandler.SpendAmountKey]);
+        availableMoney = limit - amount;
+    }
+
+    private static void GetTradeItemData(ItemStockInformation stockInfo, out string? tradeItem, out int tradeItemCount)
+    {
+        tradeItem = null;
+        tradeItemCount = 5;
+        if (stockInfo.TradeItem != null)
+        {
+            tradeItem = stockInfo.TradeItem;
+            if (stockInfo.TradeItemCount.HasValue)
+            {
+                tradeItemCount = stockInfo.TradeItemCount.Value;
+            }
+        }
     }
 }

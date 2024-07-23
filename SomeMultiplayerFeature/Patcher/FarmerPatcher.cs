4430ac673c6abf069bd539e295c4d09fb6950a99
weizinai <1776061683@qq.com>
2024-07-24 00:32:23 +0800

    修改晕倒惩罚为扣每种经验100点

--- SomeMultiplayerFeature/Patcher/FarmerPatcher.cs
+++ SomeMultiplayerFeature/Patcher/FarmerPatcher.cs
@@ -9,6 +9,8 @@ namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Patcher;
 
 internal class FarmerPatcher : BasePatcher
 {
+    private static readonly string[] SkillName = { "耕种", "钓鱼", "采集", "采矿", "战斗" };
+
     public override void Apply(Harmony harmony)
     {
         harmony.Patch(
@@ -27,6 +29,7 @@ internal class FarmerPatcher : BasePatcher
         if (which == Farmer.fishingSkill) howMuch *= 2;
     }
 
+    // 修改晕倒惩罚为扣每种经验100点
     private static IEnumerable<CodeInstruction> PerformPassoutWarpTranspiler(IEnumerable<CodeInstruction> instructions)
     {
         var codes = instructions.ToList();
@@ -42,7 +45,33 @@ internal class FarmerPatcher : BasePatcher
         if (location is FarmHouse or IslandFarmHouse or Cellar) return location;
 
         Log.Info("晕倒金钱惩罚已移除");
+
         var player = Game1.player;
+        if (player.Level < 25)
+        {
+            for (var i = 0; i < 5; i++)
+            {
+                var experience = player.experiencePoints[i];
+                var experienceDecrease = Math.Min(100, experience - GetBaseExperienceByNumber(player, i));
+                player.experiencePoints[i] -= experienceDecrease;
+                Log.Info($"{SkillName[i]}技能失去了{experienceDecrease}点经验");
+            }
+        }
+
         return Utility.getHomeOfFarmer(player);
+    }
+
+    private static int GetBaseExperienceByNumber(Farmer player, int number)
+    {
+        var level = number switch
+        {
+            0 => player.FarmingLevel,
+            1 => player.FishingLevel,
+            2 => player.ForagingLevel,
+            3 => player.MiningLevel,
+            4 => player.CombatLevel,
+            _ => -1
+        };
+        return Farmer.getBaseExperienceForLevel(level);
     }
 }

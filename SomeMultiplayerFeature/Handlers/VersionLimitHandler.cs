9a948a12b50cfd90da2a3720850b9d42b5f3f3a4
weizinai <1776061683@qq.com>
2024-07-23 20:30:55 +0800

    0.17.2

--- SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
+++ SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
@@ -9,7 +9,7 @@ namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Handlers;
 internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
 {
     private const string VersionLimitKey = ModEntry.ModDataPrefix + "VersionLimit";
-    private const string TargetVersion = "0.17.1";
+    private const string TargetVersion = "0.17.2";
 
     public VersionLimitHandler(IModHelper helper, ModConfig config)
         : base(helper, config) { }

47cf2c8424b9dced9aeb329da861ede91fcf1737
weizinai <1776061683@qq.com>
2024-07-24 17:43:30 +0800

    使用框架实现加速使用工具键响应速度

---
+++ FastControlInput/Handler/UseToolButtonHandler.cs
@@ -0,0 +1,25 @@
+using Microsoft.Xna.Framework.Input;
+using StardewValley;
+using weizinai.StardewValleyMod.FastControlInput.Framework;
+
+namespace weizinai.StardewValleyMod.FastControlInput.Handler;
+
+internal class UseToolButtonHandler : BaseInputHandler
+{
+    public UseToolButtonHandler(float multiplier) : base(multiplier) { }
+    public override bool IsEnable()
+    {
+        var currentKeyboardState = Game1.GetKeyboardState();
+        var currentMouseState = Game1.input.GetMouseState();
+        var currentGamePadState = Game1.input.GetGamePadState();
+
+        return Game1.isOneOfTheseKeysDown(currentKeyboardState, Game1.options.useToolButton) ||
+               currentMouseState.LeftButton == ButtonState.Pressed ||
+               Game1.options.gamepadControls && currentGamePadState.IsButtonDown(Buttons.X);
+    }
+
+    public override void Update()
+    {
+        this.ApplySkips(() => Game1.mouseClickPolling += Game1.currentGameTime.ElapsedGameTime.Milliseconds);
+    }
+}

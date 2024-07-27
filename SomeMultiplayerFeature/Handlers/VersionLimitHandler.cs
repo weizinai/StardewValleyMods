1edbd6cffcc77aac5da153efcea5e8721a1fab9f
weizinai <1776061683@qq.com>
2024-07-27 14:04:40 +0800

    修复某情况下玩家不被踢出的问题

--- SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
+++ SomeMultiplayerFeature/Handlers/VersionLimitHandler.cs
@@ -9,7 +9,7 @@ namespace weizinai.StardewValleyMod.SomeMultiplayerFeature.Handlers;
 internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
 {
     private const string VersionLimitKey = ModEntry.ModDataPrefix + "VersionLimit";
-    private const string TargetVersion = "0.18.1";
+    private static string TargetVersion => "0.18.1" + " " + Game1.dayOfMonth;
 
     public VersionLimitHandler(IModHelper helper, ModConfig config)
         : base(helper, config) { }
@@ -41,17 +41,22 @@ internal class VersionLimitHandler : BaseHandlerWithConfig<ModConfig>
             {
                 if (!farmer.modData.ContainsKey(VersionLimitKey) || farmer.modData[VersionLimitKey] != TargetVersion)
                 {
-                    var message = "";
-                    if (!farmer.modData.ContainsKey(VersionLimitKey))
-                        message = $"{farmer.Name}未安装<SomeMultiplayerFeature>模组，将被踢出。";
-                    else if (farmer.modData[VersionLimitKey] != TargetVersion)
-                        message = $"{farmer.Name}的模组为{farmer.modData[VersionLimitKey]}版本，要求的版本为{TargetVersion}，不满足要求，将被踢出。";
-
+                    var message = this.GetKickMessage(farmer);
                     Game1.chatBox.addInfoMessage(message);
-                    // Game1.Multiplayer.sendChatMessage(LocalizedContentManager.CurrentLanguageCode, "你的<SomeMultiplayerFeature>模组不是最新版，将被踢出。", id);
+                    Game1.Multiplayer.sendChatMessage(LocalizedContentManager.CurrentLanguageCode, message, id);
                     Game1.server?.kick(id);
                 }
             }, 5000);
         }
+    }
+
+    private string GetKickMessage(Farmer farmer)
+    {
+        var actualCurrentVersion = ArgUtility.SplitBySpace(farmer.modData[VersionLimitKey])[0];
+        var actualTargetVersion = ArgUtility.SplitBySpace(TargetVersion)[0];
+
+        return !farmer.modData.ContainsKey(VersionLimitKey)
+            ? $"{farmer.Name}未安装<SomeMultiplayerFeature>模组，将被踢出。"
+            : $"{farmer.Name}的<SomeMultiplayerFeature>模组为<{actualCurrentVersion}>版本，要求的版本为<{actualTargetVersion}>，不满足要求，将被踢出。";
     }
 }

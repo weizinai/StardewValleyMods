e7b2355bf4ba2a58f1aa8e8072ef05f2dfb508a9
weizinai <1776061683@qq.com>
2024-08-02 01:03:13 +0800

    添加注释

--- MultiplayerModLimit/Framework/ModConfig.cs
+++ MultiplayerModLimit/Framework/ModConfig.cs
@@ -2,11 +2,16 @@ namespace weizinai.StardewValleyMod.MultiplayerModLimit.Framework;
 
 internal class ModConfig
 {
+    // 一般设置
     public bool EnableMod { get; set; } = true;
     public int KickPlayerDelayTime { get; set; } = 3;
     public bool SendSMAPIInfo { get; set; } = true;
+
+    // 限制设置
     public bool RequireSMAPI { get; set; } = true;
     public LimitMode LimitMode { get; set; } = LimitMode.WhiteListMode;
+
+    // 选择的模组列表设置
     public string AllowedModListSelected { get; set; } = "Default";
     public string RequiredModListSelected { get; set; } = "Default";
     public string BannedModListSelected { get; set; } = "Default";

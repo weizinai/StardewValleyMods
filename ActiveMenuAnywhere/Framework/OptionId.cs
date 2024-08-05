b3beb0dcd5ed462363227148f0a5425189a67bf3
weizinai <1776061683@qq.com>
2024-08-05 19:06:55 +0800

    添加OptionId枚举

---
+++ ActiveMenuAnywhere/Framework/OptionId.cs
@@ -0,0 +1,74 @@
+namespace weizinai.StardewValleyMod.ActiveMenuAnywhere.Framework;
+
+internal enum OptionId
+{
+    // Beach
+    Bobber,
+    DecorationBoat,
+    MagicBoat,
+    NightMarketTraveler,
+    Willy,
+    // Desert
+    BuyQiCoin,
+    Casino,
+    ClubSeller,
+    DesertTrade,
+    FarmerFile,
+    Sandy,
+    // Farm
+    ShippingBin,
+    TV,
+    // Forest
+    HatMouse,
+    MarineOption,
+    Raccoon,
+    Traveler,
+    Wizard,
+    // GingerIsland
+    ForgeOption,
+    IslandResort,
+    IslandTrade,
+    QiCat,
+    QiGemShop,
+    QiSpecialOrder,
+    VolcanoShop,
+    // Mountain
+    Dwarf,
+    Marlon,
+    Monster,
+    Robin,
+    // RSV
+    Ian,
+    Jeric,
+    Joi,
+    Kimpoi,
+    Lola,
+    Lorenzo,
+    MysticFall1,
+    MysticFall2,
+    MysticFall3,
+    NinjaBoard,
+    Paula,
+    Pika,
+    RSVQuestBoard,
+    RSVSpecialOrder,
+    // SVE
+    Sophia,
+    // Town
+    AbandonedJojaMart,
+    Billboard,
+    Bookseller,
+    Client,
+    CommunityCenter,
+    DyeOption,
+    GusOption,
+    HarveyOption,
+    IceCreamStand,
+    JojaShop,
+    Krobus,
+    Pierre,
+    PrizeTicket,
+    SpecialOrder,
+    Status,
+    Tailoring
+}

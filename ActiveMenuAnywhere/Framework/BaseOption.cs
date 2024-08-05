2b4d88ffde3c9a6db05f7e789e2f2a4030903464
weizinai <1776061683@qq.com>
2024-08-05 18:13:14 +0800

    将只读属性修改为私有字段

--- ActiveMenuAnywhere/Framework/BaseOption.cs
+++ ActiveMenuAnywhere/Framework/BaseOption.cs
@@ -6,14 +6,14 @@ namespace weizinai.StardewValleyMod.ActiveMenuAnywhere.Framework;
 
 internal abstract class BaseOption
 {
-    public string Label { get; }
-    public Rectangle SourceRect { get; }
+    private readonly string label;
+    private readonly Rectangle sourceRect;
     public float Scale { get; set; } = 1f;
 
     protected BaseOption(string label, Rectangle sourceRect)
     {
-        this.Label = label;
-        this.SourceRect = sourceRect;
+        this.label = label;
+        this.sourceRect = sourceRect;
     }
 
     public virtual bool IsEnable()
@@ -25,7 +25,7 @@ internal abstract class BaseOption
 
     public void Draw(SpriteBatch b, Texture2D texture, int x, int y)
     {
-        b.Draw(texture, new Vector2(x + 100, y + 100), this.SourceRect, Color.White, 0f, new Vector2(100, 100), this.Scale, SpriteEffects.None, 0f);
-        DrawHelper.DrawTab(x + 100, y + 120, Game1.smallFont, this.Label, Align.Center);
+        b.Draw(texture, new Vector2(x + 100, y + 100), this.sourceRect, Color.White, 0f, new Vector2(100, 100), this.Scale, SpriteEffects.None, 0f);
+        DrawHelper.DrawTab(x + 100, y + 120, Game1.smallFont, this.label, Align.Center);
     }
 }

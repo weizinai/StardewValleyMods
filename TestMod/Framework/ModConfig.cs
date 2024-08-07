266389b9e205498a78194d544c88900db6fcf2da
weizinai <1776061683@qq.com>
2024-08-07 13:59:00 +0800

    添加注释和更新文档

--- TestMod/Framework/ModConfig.cs
+++ TestMod/Framework/ModConfig.cs
@@ -11,7 +11,10 @@ internal class ModConfig
         Instance = helper.ReadConfig<ModConfig>();
     }
 
+    // 齐瓜牌概率
     public SingleValueConfig<float> CardChance { get; set; } = new(true, 0.005f);
+
+    // 轮盘旋转速度
     public SingleValueConfig<int> WheelSpinSpeed { get; set; } = new(true, 0);
     public bool ExtraSpeed { get; set; }
 }
--- TestMod/docs/CHANGELOG.md
+++ TestMod/docs/CHANGELOG.md
@@ -1,4 +1,4 @@
 # 0.1.0
 
-- 添加ChangeCardChance功能，让玩家可以修改齐瓜牌出现的概率。
+- 添加`ChangeCardChance`功能，让玩家可以修改齐瓜牌出现的概率。
 - 添加`WheelSpinSpeed`功能，让玩家可以自定义轮盘初始旋转速度。

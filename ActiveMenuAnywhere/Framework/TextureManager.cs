9a9195c68072dad98f4e284147fed4d5cb6fb06b
weizinai <1776061683@qq.com>
2025-05-10 18:07:16 +0800

    Refactor TextureManager to use a private readonly field
    Replaced the `Textures` property with a private readonly dictionary `textures` for better encapsulation. Updated all references to use the new field while maintaining the same functionality.

--- ActiveMenuAnywhere/Framework/TextureManager.cs
+++ ActiveMenuAnywhere/Framework/TextureManager.cs
@@ -8,27 +8,27 @@ public class TextureManager
 {
     public static TextureManager Instance { get; } = new();
 
-    private Dictionary<MenuTabId, Texture2D> Textures { get; } = new();
-    public Texture2D FarmTexture => this.Textures[MenuTabId.Farm];
-    public Texture2D TownTexture => this.Textures[MenuTabId.Town];
-    public Texture2D MountainTexture => this.Textures[MenuTabId.Mountain];
-    public Texture2D ForestTexture => this.Textures[MenuTabId.Forest];
-    public Texture2D BeachTexture => this.Textures[MenuTabId.Beach];
-    public Texture2D DesertTexture => this.Textures[MenuTabId.Desert];
-    public Texture2D GingerIslandTexture => this.Textures[MenuTabId.GingerIsland];
-    public Texture2D SVETexture => this.Textures[MenuTabId.SVE];
-    public Texture2D RSVTexture => this.Textures[MenuTabId.RSV];
+    private readonly Dictionary<MenuTabId, Texture2D> textures  = new();
+    public Texture2D FarmTexture => this.textures[MenuTabId.Farm];
+    public Texture2D TownTexture => this.textures[MenuTabId.Town];
+    public Texture2D MountainTexture => this.textures[MenuTabId.Mountain];
+    public Texture2D ForestTexture => this.textures[MenuTabId.Forest];
+    public Texture2D BeachTexture => this.textures[MenuTabId.Beach];
+    public Texture2D DesertTexture => this.textures[MenuTabId.Desert];
+    public Texture2D GingerIslandTexture => this.textures[MenuTabId.GingerIsland];
+    public Texture2D SVETexture => this.textures[MenuTabId.SVE];
+    public Texture2D RSVTexture => this.textures[MenuTabId.RSV];
 
     public void LoadTexture(IModHelper helper)
     {
-        this.Textures.Add(MenuTabId.Farm, helper.ModContent.Load<Texture2D>("assets/Farm.png"));
-        this.Textures.Add(MenuTabId.Town, helper.ModContent.Load<Texture2D>("assets/Town.png"));
-        this.Textures.Add(MenuTabId.Mountain, helper.ModContent.Load<Texture2D>("assets/Mountain.png"));
-        this.Textures.Add(MenuTabId.Forest, helper.ModContent.Load<Texture2D>("assets/Forest.png"));
-        this.Textures.Add(MenuTabId.Beach, helper.ModContent.Load<Texture2D>("assets/Beach.png"));
-        this.Textures.Add(MenuTabId.Desert, helper.ModContent.Load<Texture2D>("assets/Desert"));
-        this.Textures.Add(MenuTabId.GingerIsland, helper.ModContent.Load<Texture2D>("assets/GingerIsland.png"));
-        this.Textures.Add(MenuTabId.RSV, helper.ModContent.Load<Texture2D>("assets/RSV.png"));
-        this.Textures.Add(MenuTabId.SVE, helper.ModContent.Load<Texture2D>("assets/SVE.png"));
+        this.textures.Add(MenuTabId.Farm, helper.ModContent.Load<Texture2D>("assets/Farm.png"));
+        this.textures.Add(MenuTabId.Town, helper.ModContent.Load<Texture2D>("assets/Town.png"));
+        this.textures.Add(MenuTabId.Mountain, helper.ModContent.Load<Texture2D>("assets/Mountain.png"));
+        this.textures.Add(MenuTabId.Forest, helper.ModContent.Load<Texture2D>("assets/Forest.png"));
+        this.textures.Add(MenuTabId.Beach, helper.ModContent.Load<Texture2D>("assets/Beach.png"));
+        this.textures.Add(MenuTabId.Desert, helper.ModContent.Load<Texture2D>("assets/Desert"));
+        this.textures.Add(MenuTabId.GingerIsland, helper.ModContent.Load<Texture2D>("assets/GingerIsland.png"));
+        this.textures.Add(MenuTabId.RSV, helper.ModContent.Load<Texture2D>("assets/RSV.png"));
+        this.textures.Add(MenuTabId.SVE, helper.ModContent.Load<Texture2D>("assets/SVE.png"));
     }
 }
